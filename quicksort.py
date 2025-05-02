ec24aaa4ce01c755768939a8da6c847820b4230f
s298157 <s298157@studenti.polito.it>
2025-05-02 15:46:51 +0200

    Update

--- quicksort.py
+++ quicksort.py
@@ -1,30 +1,24 @@
+# Creazione di un metodo Quicksort che ordini una lista di numeri (file Ricorsione, slide 45-46-47-48)
 
-def quicksort(sequenza):
-    if len(sequenza) == 1:
+def quicksortRicorsivo(sequenza):
+    if len(sequenza) <= 1:
         return sequenza
     else:
-        #1) scegliere pivot
-        pivot = sequenza[0]
-        #2) dividere sequenza in sottosequenze
-        # sequenza_smaller = []
-        # sequenza_larger = []
-        # sequenza_pivot =[]
-        # for i in sequenza:
-        #     if(i<pivot):
-        #         sequenza_smaller.append(i)
-        #     elif (i>pivot):
-        #         sequenza_larger.append(i)
-        #     else:
-        #         sequenza_pivot.append(i)
-
-        sequenza_smaller = [n for n in sequenza if n < pivot]
-        sequenza_larger = [n for n in sequenza if n > pivot]
-        sequenza_pivot = [n for n in sequenza if n == pivot]
-        return (quicksort(sequenza_smaller)+
-                sequenza_pivot+
-                quicksort(sequenza_larger))
-
+        # 1. determinare un pivot
+        pivot = sequenza[0] # scelto casualmente dato che non si hanno informazioni sui dati
+        # 2. dividere la sequenza in sotto-sequenze
+        sequenza_smaller = []
+        sequenza_larger = []
+        sequenza_pivot = []
+        for i in sequenza:
+            if i < pivot:
+                sequenza_smaller.append(i)
+            elif i > pivot:
+                sequenza_larger.append(i)
+            else:
+                sequenza_pivot.append(i)
+        return quicksortRicorsivo(sequenza_smaller) + sequenza_pivot + quicksortRicorsivo(sequenza_larger)
 
 if __name__ == '__main__':
     sequenza = [3, 5, 2, 9, 11, 4, 7]
-    print(quicksort(sequenza))
+    print(quicksortRicorsivo(sequenza))
