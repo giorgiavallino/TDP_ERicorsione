1bb1a69ef410f67987594b01c3fc423e007d23c5
s298157 <s298157@studenti.polito.it>
2025-05-26 19:48:48 +0200

    Update

--- quadrato_magico.py
+++ quadrato_magico.py
@@ -1,21 +1,22 @@
+# Creazione di un metodo ricorsivo che crea un quadrato magico con i rispettivi valori al suo interno
 
 def quadrato_magico(N):
     rimanenti = list(range(1,N*N+1))
     ricorsione([], rimanenti, N)
 
-def ricorsione(parziale, rimanenti, N):
-    #condizione terminale
-    if len(parziale) == N*N:
-        print(parziale)
-    #caso non terminale
+def ricorsione(soluzione_parziale, rimanenti, N):
+    # Condizione terminale --> se la griglia è costituita da N*N elementi, allora la griglia è stata completata (al
+    # posto di visualizzare una griglia, nel caso proposto essa sarà rappresentata da una lista)
+    if len(soluzione_parziale) == N*N:
+        print(soluzione_parziale)
+    # Caso non terminale / ricorsivo
     else:
         for i in range(len(rimanenti)):
             numero = rimanenti[i]
-            parziale.append(numero)
+            soluzione_parziale.append(numero)
             nuovi_rimanenti = rimanenti[:i] + rimanenti[i+1:]
-            ricorsione(parziale, nuovi_rimanenti, N)
-            parziale.pop()
-
+            ricorsione(soluzione_parziale, nuovi_rimanenti, N)
+            soluzione_parziale.pop()
 
 if __name__ == '__main__':
     quadrato_magico(3)
